5658c9a010bd5c0270eec0c502330a552c089b78
Mina <minakinamoto@gmail.com>
2020-08-26 14:17:21 +0700

    add customer form

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,6 +2,9 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.ArrayList;
 
@@ -10,18 +13,26 @@ import java.util.ArrayList;
 @Controller
 public class CustomerController {
 
-    @RequestMapping("/customer")
-    public String getCustomerPage(Model model) {
+    ArrayList<Customer> customers = new ArrayList<>();
 
-        ArrayList<Customer> customers = new ArrayList<>();
-        customers.add(new Customer(1,"Peter","1234"));
-        customers.add(new Customer(2,"Nancy","2345"));
-        customers.add(new Customer(3,"Rick","3456"));
+    @GetMapping ("/customer")
+    public String getCustomerPage(Model model) {
 
         model.addAttribute("allCustomers", customers);
 
         return "customer";
     }
-}
+
+    @PostMapping ("/customer")
+    public String addNewCustomer(@ModelAttribute Customer customer, Model model)
+    {
+            customers.add(customer);
+            model.addAttribute("allCustomers", customers);
+            return "redirect:customer";
+
+    }
+
+ }
+
 
 
